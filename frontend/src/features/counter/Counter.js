95956e1cf46c29a96fbed57e481766031fb569ff
kaju111 <saikiakaju275@gmail.com>
2023-12-12 15:54:48 +0530

    first commit

---
+++ frontend/src/features/counter/Counter.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, incrementAsync, selectCount } from "./counterSlice";
+
+export default function Counter() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+
+  const incrementValue = Number(incrementAmount) || 0;
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
